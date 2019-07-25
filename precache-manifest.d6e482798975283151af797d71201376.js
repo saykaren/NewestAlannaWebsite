ec0d1321fe42ef4a12e8c55ceec2f5563ecd58cc
Karen Garcia <karen.garcia@nexscreen.com>
2019-07-25 08:42:42 -0600

    Updates

---
+++ precache-manifest.d6e482798975283151af797d71201376.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebabda495f13dfa6ffaffd9db01c1b8",
+    "url": "/saykaren/NewestAlannaWebsite/index.html"
+  },
+  {
+    "revision": "71deb28db859d71e7153",
+    "url": "/saykaren/NewestAlannaWebsite/static/css/main.3e761324.chunk.css"
+  },
+  {
+    "revision": "5b78da2dc9bb2a847546",
+    "url": "/saykaren/NewestAlannaWebsite/static/js/2.631e91b0.chunk.js"
+  },
+  {
+    "revision": "71deb28db859d71e7153",
+    "url": "/saykaren/NewestAlannaWebsite/static/js/main.a1c11e81.chunk.js"
+  },
+  {
+    "revision": "4ed9ab0740c44ae4f5d7",
+    "url": "/saykaren/NewestAlannaWebsite/static/js/runtime~main.92c88a4b.js"
+  },
+  {
+    "revision": "6704307ad28acf81202b0801ec841261",
+    "url": "/saykaren/NewestAlannaWebsite/static/media/alanna_heroPic.6704307a.jpg"
+  },
+  {
+    "revision": "efd8a1fa8218be31598ed9d815697dd4",
+    "url": "/saykaren/NewestAlannaWebsite/static/media/reference_Karen.efd8a1fa.jpg"
+  }
+]);
